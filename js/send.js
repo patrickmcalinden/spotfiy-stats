c29942d7846bdad5abe1fced0f23c0ef4de86f2d
Patrick McAlinen <patrickmcalinden226@gmail.com>
2021-09-24 03:08:40 -0400

    main

--- js/send.js
+++ js/send.js
@@ -4,7 +4,7 @@
 
 var client_id = 'ffd460a77ebb49b5a874ff0008cfada0';
 var client_secret = 'https://accounts.spotify.com/authorize'; 
-var redirect_uri = 'https://patrickmcalinden.github.io/spotfiy-stats/stats.html'; 
+var redirect_uri = 'http://127.0.0.1:5500/stats.html'; 
 var joiner = "%20"
 
 var scopes = ["user-top-read"]
