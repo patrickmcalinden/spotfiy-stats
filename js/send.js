b7513de190fc9f52f18bd1af381821fd68e6e571
Patrick McAlinen <patrickmcalinden226@gmail.com>
2021-10-03 23:10:22 -0400

    main

--- js/send.js
+++ js/send.js
@@ -1,10 +1,10 @@
 //sends user to spotify log-in link
 //Using implicit grant Flow *Does not use client Secret...no server app needed*
 //https://developer.spotify.com/documentation/general/guides/authorization-guide/
-
+//https://patrickmcalinden.github.io/spotfiy-stats/stats.html
 var client_id = 'ffd460a77ebb49b5a874ff0008cfada0';
 var client_secret = 'https://accounts.spotify.com/authorize'; 
-var redirect_uri = 'https://patrickmcalinden.github.io/spotfiy-stats/stats.html'; 
+var redirect_uri = 'http://127.0.0.1:5500/stats.html'; 
 var joiner = "%20"
 
 var scopes = ["user-top-read"]
